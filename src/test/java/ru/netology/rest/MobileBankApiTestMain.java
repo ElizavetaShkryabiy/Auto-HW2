d5c78aae66ddaedf6f25b73c7b5d3df0d00265e3
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-01 09:48:25 +0400

    repeated tests deleted

--- src/test/java/ru/netology/rest/MobileBankApiTestMain.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestMain.java
@@ -1,7 +1,9 @@
 package ru.netology.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,6 +11,27 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestMain {
+    private RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setBasePath("/api/v1")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @Test
+    void shouldReturnDemoAccountsWithVariable() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
     @Test
     void shouldReturnDemoAccounts() {
         given()
@@ -21,6 +44,7 @@ class MobileBankApiTestMain {
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("every{ it.balance >= 0 }", is(true))
         ;
     }
 }
