719c113703dc59fbb7000da66f27c68ad56e144a
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-01 08:31:24 +0400

    comments deleted

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -8,15 +8,11 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
+           given()
           .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
+          .when()
           .get("/demo/accounts")
-      // Проверки
-      .then()
+           .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
