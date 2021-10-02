3bb9c168b0a8b8fd0794455cadf0887e6ff2c3ea
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-10-02 14:39:58 +0400

    repeated tests deleted

--- src/test/java/ru/netology/rest/MobileBankApiTestMain.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestMain.java
@@ -1,9 +1,4 @@
 package ru.netology.rest;
-
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,27 +6,6 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestMain {
-    private RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setBasePath("/api/v1")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @Test
-    void shouldReturnDemoAccountsWithVariable() {
-        given()
-                .spec(requestSpec)
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
-    }
-
     @Test
     void shouldReturnDemoAccounts() {
         given()
